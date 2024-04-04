471bf2ad6a904514ad1b26b70950a0765caa7497
ishimwe <gabriella.ishimwe.etu@univ-lille.fr>
2024-04-05 00:31:06 +0200

    lecture fichier et distribution des taches a continuer

--- demo/src/main/java/fr/lille/akka/akkaService/AkkaService.java
+++ demo/src/main/java/fr/lille/akka/akkaService/AkkaService.java
@@ -1,33 +1,116 @@
 
-package com.example.demo.akkaService;
+package fr.lille.akka.akkaService;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
+import fr.lille.akka.actor.MapperActor;
+import fr.lille.akka.actor.ReducerActor;
+import fr.lille.akka.message.CleanReducerMessage;
+import fr.lille.akka.message.CountRequest;
+import fr.lille.akka.message.MapReduceMessage;
 
 @Service
 
 public class AkkaService {
 	
-    private static final ActorSystem system = ActorSystem.create("MapReduceSystem");
+  
+    private ActorRef mapper1, mapper2, mapper3, red1, red2;
+	private ActorSystem aSystem,system2;
+	private Map<String,ActorRef> reducers;
+	private boolean initialized=false;
+	private File fichier;
+	private boolean dispatched;
+	
+	private BufferedReader bReader;
+	private List<String> ALLOWED_EXTENSION ;
+    
 
-    private static final ActorRef[] mappers = new ActorRef[3];
-    private static final ActorRef[] reducers = new ActorRef[2];
-    private static List<ActorRef>reducerTabs;
     
-    
-    
-    public static void initialize() {
-    	reducerTabs=new ArrayList <> ();
-        for (int i = 0; i < 2; i++) {
-            reducers[i] = system.actorOf(Props.create(Reducer.class), "reducer" + i);
-            reducerTabs.add(reducers[i]);
-        }
-        
-        for (int i = 0; i < 3; i++) {
-            mappers[i] = system.actorOf(Props.create(Mapper.class,reducerTabs), "mapper" + i);
-        }
+    public void initialize() {
+        aSystem = ActorSystem.create("aSystem", ConfigFactory.load("resources/application1.conf"));
+		system2 = ActorSystem.create("system2",  ConfigFactory.load("resources/application2.conf"));
+		
+		this.mapper1 = aSystem.actorOf(Props.create(MapperActor.class), "mapper1");
+		this.mapper2 = system2.actorOf(Props.create(MapperActor.class), "mapper2");
+		this.mapper3 = aSystem.actorOf(Props.create(MapperActor.class), "mapper3");
+		
+		ActorRef reducerConsonants, reducerVowels;
+		reducerConsonants = system2.actorOf(Props.create(ReducerActor.class), "reducerConsonants");
+		reducerVowels = aSystem.actorOf(Props.create(ReducerActor.class), "reducerVowels");
+		
+		this.reducers = new HashMap<>();
+		this.reducers.put("vowels", reducerVowels);
+		this.reducers.put("consonants", reducerConsonants);
+		
+		this.initialized = true;
+		this.initialized = false;
+		
+		ALLOWED_EXTENSION = new ArrayList<>();
+		ALLOWED_EXTENSION.add("txt");
+		ALLOWED_EXTENSION.add("csv");
 
     }
-    //à continuer
+
+    private void cleanReducers() throws TimeoutException {
+		
+		final Inbox inbox = Inbox.create(aSystem);
+		CleanReducerMessage msg = new CleanReducerMessage(true);
+        inbox.send(this.mapper1,  msg );
+        
+        final Object response = inbox.receive(Duration.create(5, TimeUnit.SECONDS));
+        
+        this.aSystem.stop(inbox.getRef());
+		
+		reducers.get("vowels").tell(msg, ActorRef.noSender());
+		reducers.get("consonants").tell( msg, ActorRef.noSender());
+	}
+
+    public void setReader(MultipartFile file) throws IOException {
+		String fileName = file.getOriginalFilename();
+		 
+		  if( !file.isEmpty() && ALLOWED_EXTENSION.contains( getFileExtension(fileName)) ) {
+			  InputStream inputStream = file.getInputStream();
+			  bReader =  new BufferedReader(new InputStreamReader(inputStream));
+		  }
+		 
+	}
+	private String getFileExtension( String fileName) {
+	    return fileName != null ? fileName.substring(fileName.lastIndexOf(".") + 1) : null;
+	}
+    @PreDestroy
+    //liberer les ressources avant la destruction du service
+	public void clean() {
+		if(this.aSystem==null || this.system2==null) {return;}
+		this.aSystem.stop(mapper1);
+		this.aSystem.stop(mapper3);
+		this.aSystem.stop(this.reducers.get("vowels"));
+		
+		
+		this.system2.stop(mapper2);
+		this.system2.stop(this.reducers.get("consonants"));
+		
+		
+		this.aSystem.shutdown();
+		this.system2.shutdown();
+		this.dispatched=false;
+		this.initialized=false;
+	}
+    public boolean isDispatched() {
+		return dispatched;
+	}
+
+	public void setDispatched(boolean dispatched) {
+		this.dispatched = dispatched;
+	}
+
+	public boolean isInitialized() {
+		return initialized;
+	}
+
+	public void setInitialized(boolean initialized) {
+		this.initialized = initialized;
+	}
+
+     
 }
--- demo/src/main/java/fr/lille/akka/controller/AkkaController.java
+++ demo/src/main/java/fr/lille/akka/controller/AkkaController.java
@@ -1,13 +1,13 @@
-package demo.controllers;
+package fr.lille.akka.controllers;
 
 import akka.actor.ActorRef;
 import akka.util.Timeout;
 
-import demo.message.*;
+import akka.message.*;
 
-import demo.actors.Mapper;
-import demo.actors.Reducer;
-import demo.akkaService.*;
+import akka.actors.MapperActor;
+import akka.actors.ReducerActor;
+import akka.akkaService.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -55,6 +55,47 @@ public class AkkaController {
 		 
 		return "home";
 	}
+
+	@PostMapping(value = "/dispatch", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public String dispatchLines(@RequestParam("file") MultipartFile file, Model model) throws IOException {
+
+		akkaService.setReader(file);
+		akkaService.dispatchLinesToMapper();
+		model.addAttribute("dispatched", akkaService.isDispatched());
+		return "home";
+	}
+
+	public void dispatchLinesToMapper() throws IOException {
+		 
+		 
+		String text;
+		MapReduceMessage mapReduceMessage ;
+		int i = 1;
+		while( (text = bReader.readLine() ) != null) {
+			mapReduceMessage = new MapReduceMessage(reducers, text);
+			
+			switch(i) {
+				case 1:
+					this.mapper1.tell(mapReduceMessage, ActorRef.noSender());
+					break;
+				case 2:
+					this.mapper2.tell(mapReduceMessage, ActorRef.noSender());
+					break;
+				case 3:
+					this.mapper3.tell(mapReduceMessage, ActorRef.noSender()); 
+					break;
+				default:
+					break;
+			}
+			
+			i = i==3 ? 1 : i+1;
+		}
+	   
+		bReader.close();
+		this.dispatched=true;
+	   
+   }
+
 }
 
 
